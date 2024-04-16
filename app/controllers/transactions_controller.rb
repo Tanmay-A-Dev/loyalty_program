787e9c9a34b62d6660737b06a79d2883ccd8c248
Tanmay <tanmayagnihotri75@gmail.com>
2024-04-16 13:49:03 +0530

    added transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,28 @@
 class TransactionsController < ApplicationController
-end
+    def index
+      @user = User.find(params[:user_id])
+      @transactions = @user.transactions
+    end
+  
+    def new
+      @user = User.find(params[:user_id])
+      @transaction = @user.transactions.new
+    end
+  
+    def create
+      @user = User.find(params[:user_id])
+      @transaction = @user.transactions.new(transaction_params)
+      if @transaction.save
+        redirect_to user_transactions_path(@user), notice: 'Transaction was successfully recorded.'
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def transaction_params
+      params.require(:transaction).permit(:amount, :foreign_transaction)
+    end
+  end
+  
