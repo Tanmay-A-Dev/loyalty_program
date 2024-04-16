988ed2e191b445425c399840c5435595f6e44e4a
Tanmay <tanmayagnihotri75@gmail.com>
2024-04-16 16:12:45 +0530

    Added logic for coffee rewards

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,17 @@
 class Transaction < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
+
+  after_create :calculate_points_and_check_rewards_eligibility
+
+  scope :in_month, ->(month) { where("extract(month from created_at) = ?", month) }
+
+  private
+
+  def calculate_points_and_check_rewards_eligibility
+    points = amount / 100
+    points *= 2 if foreign_transaction?
+    user.update_points(points)
+    user.check_rewards_eligibility(points)
+    user.update_monthly_points(created_at.month)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,41 @@
 class User < ApplicationRecord
-    has_many :transactions
-    has_many :rewards
+  has_many :transactions
+  has_many :rewards
+
+  enum tier: { standard: 0, gold: 1, platinum: 2 }
+
+  def birthday_month
+    birthday.month
+  end
+
+  def update_points(points)
+    update(points: self.points + points)
+    update_tier
+  end
+
+  def check_rewards_eligibility(points)
+    check_free_coffee_reward(points)
+    check_free_coffee_birthday_reward
+  end
+
+  def update_monthly_points(month)
+    transactions_this_month = transactions.in_month(month)
+    monthly_points = transactions_this_month.sum(&:points)
+    update(monthly_points: monthly_points)
+    check_free_coffee_reward(monthly_points)
+  end
+
+  private
+
+  def check_free_coffee_reward(points)
+    if points >= 100
+      rewards.create(name: 'Free Coffee')
+    end
+  end
+
+  def check_free_coffee_birthday_reward
+    if birthday_month == Date.today.month
+      rewards.create(name: 'Free Coffee (Birthday)')
+    end
+  end
 end
